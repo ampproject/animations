5d68829be0c23f1467359031f96353bae0fa90b2
Sepand Parhami <sparhami@google.com>
2018-10-18 18:16:43 -0700

    Review comments.

--- src/test-imitation-img.ts
+++ src/test-imitation-img.ts
@@ -20,6 +20,10 @@ import {imgLoadPromise} from './testing/utils';
 const {expect} = chai;
 const fourByThreeUri = 'data:image/gif;base64,R0lGODdhBAADAIAAAP///////ywAAAAABAADAAACA4SPVgA7';
 const threeByFourUri = 'data:image/gif;base64,R0lGODdhAwAEAIAAAP///////ywAAAAAAwAEAAACA4SPVgA7';
+const testLeft = 0;
+const testTop = 0;
+const testWidth = 10;
+const testHeight = 10;
 
 async function updateImg(img, fit, src) {
   img.style.objectFit = fit;
@@ -29,12 +33,12 @@ async function updateImg(img, fit, src) {
 
 describe('createImitationImg', () => {
   let testContainer;
-  let img;
+  let srcImg;
 
   beforeEach(() => {
-    img = document.createElement('img');
+    srcImg = document.createElement('img');
     testContainer = document.createElement('div');
-    testContainer.appendChild(img);
+    testContainer.appendChild(srcImg);
     document.body.appendChild(testContainer);
   });
 
@@ -44,46 +48,49 @@ describe('createImitationImg', () => {
 
   describe('for object-fit: contain', () => {
     beforeEach(async () => {
-      await updateImg(img, 'contain', threeByFourUri);
-      img.style.width = '10px';
-      img.style.height = '10px';
+      await updateImg(srcImg, 'contain', threeByFourUri);
+      srcImg.style.width = `${testWidth}px`;
+      srcImg.style.height = `${testHeight}px`;
     });
 
     describe('the scaleElement', () => {
       it('should size correctly', async () => {
-        const {scaleElement} = createImitationImg(img);
+        const {scaleElement} = createImitationImg(srcImg);
         testContainer.appendChild(scaleElement);
 
         const {width, height} = scaleElement.getBoundingClientRect();
-        expect(width).to.equal(10);
-        expect(height).to.equal(10);
+        expect(width).to.equal(testWidth);
+        expect(height).to.equal(testHeight);
       });
     });
 
     describe('the img', () => {
-      let scaleElement;
-      let replacementImg;
+      let imgElement;
 
       beforeEach(() => {
-        const replacement = createImitationImg(img);
-        scaleElement = replacement.scaleElement;
-        replacementImg = replacement.img;
+        const {img, scaleElement} = createImitationImg(srcImg);
+        imgElement = img;
         testContainer.appendChild(scaleElement);
         scaleElement.style.position = 'fixed';
-        scaleElement.style.top = '10px';
-        scaleElement.style.left = '10px';
+        scaleElement.style.top = `${testTop}px`;
+        scaleElement.style.left = `${testLeft}px`;
       })
 
       it('should size correctly', async () => {
-        const {width, height} = replacementImg.getBoundingClientRect();
+        const {width, height} = imgElement.getBoundingClientRect();
+        // (3/4) * testHeight
         expect(width).to.equal(7.5);
+        // Larger dimension is height, so should match testHeight
         expect(height).to.equal(10);
       });
 
       it('should position correctly', async () => {
-        const {left, top} = replacementImg.getBoundingClientRect();
-        expect(left).to.equal(11.25);
-        expect(top).to.equal(10);
+        const {left, top} = imgElement.getBoundingClientRect();
+        // Should center the horizontal and has a width of 7.5
+        // testLeft + (testWidth - ((3/4) * testWidth)) / 2 = 1.25
+        expect(left).to.equal(1.25);
+        // Height matches testHeight, so centered at testTop
+        expect(top).to.equal(0);
       });
     });
   });
@@ -91,68 +98,76 @@ describe('createImitationImg', () => {
   describe('for object-fit: cover', () => {
     describe('for portrait images', () => {
       beforeEach(async () => {
-        await updateImg(img, 'cover', threeByFourUri);
-        img.style.width = '10px';
-        img.style.height = '10px';
+        await updateImg(srcImg, 'cover', threeByFourUri);
+        srcImg.style.width = `${testWidth}px`;
+        srcImg.style.height = `${testHeight}px`;
       });
   
       describe('the img', () => {
-        let replacementImg;
+        let imgElement;
   
         beforeEach(() => {
-          const replacement = createImitationImg(img);
-          const scaleElement = replacement.scaleElement;
-          replacementImg = replacement.img;
+          const {img, scaleElement} = createImitationImg(srcImg);
+          imgElement = img;
           testContainer.appendChild(scaleElement);
           scaleElement.style.position = 'fixed';
-          scaleElement.style.top = '100px';
-          scaleElement.style.left = '100px';
+          scaleElement.style.top = `${testTop}px`;
+          scaleElement.style.left = `${testLeft}px`;
         })
   
         it('should size correctly', async () => {
-          const {width, height} = replacementImg.getBoundingClientRect();
+          const {width, height} = imgElement.getBoundingClientRect();
+          // Smaller dimension is width, so should match testWidth
           expect(width).to.equal(10);
+          // (4/3) * testWidth
           expect(height).to.be.closeTo(13.333, 0.1);
         });
   
         it('should position correctly', async () => {
-          const {left, top} = replacementImg.getBoundingClientRect();
-          expect(left).to.equal(100);
-          expect(top).to.be.closeTo(98.333, 0.1);
+          const {left, top} = imgElement.getBoundingClientRect();
+          // Width matches testWidth, so centered at testLeft
+          expect(left).to.equal(0);
+          // Should center the vertical and has a height of 13.333
+          // testLeft + (testWidth - ((4/3) * testWidth)) / 2 = -1.666
+          expect(top).to.be.closeTo(-1.666, 0.1);
         });
       });
     });
 
     describe('for landscape images', () => {
       beforeEach(async () => {
-        await updateImg(img, 'cover', fourByThreeUri);
-        img.style.width = '10px';
-        img.style.height = '10px';
+        await updateImg(srcImg, 'cover', fourByThreeUri);
+        srcImg.style.width = `${testWidth}px`;
+        srcImg.style.height = `${testHeight}px`;
       });
 
       describe('the img', () => {
-        let replacementImg;
+        let imgElement;
 
         beforeEach(() => {
-          const replacement = createImitationImg(img);
-          const scaleElement = replacement.scaleElement;
-          replacementImg = replacement.img;
+          const {img, scaleElement} = createImitationImg(srcImg);
+          imgElement = img;
           testContainer.appendChild(scaleElement);
           scaleElement.style.position = 'fixed';
-          scaleElement.style.top = '100px';
-          scaleElement.style.left = '100px';
+          scaleElement.style.top = `${testTop}px`;
+          scaleElement.style.left = `${testLeft}px`;
         })
 
         it('should size correctly', async () => {
-          const {width, height} = replacementImg.getBoundingClientRect();
+          const {width, height} = imgElement.getBoundingClientRect();
+          // (4/3) * testHeight = 13.333
           expect(width).to.be.closeTo(13.333, 0.1);
+          // Smaller dimension is height, so should match testHeight
           expect(height).to.equal(10);
         });
 
         it('should position correctly', async () => {
-          const {left, top} = replacementImg.getBoundingClientRect();
-          expect(left).to.be.closeTo(98.333, 0.1);
-          expect(top).to.equal(100);
+          const {left, top} = imgElement.getBoundingClientRect();
+          // Should center the horizontal and has a width of 13.333
+          // testLeft + (testHeight - ((4/3) * testHeight)) / 2 = -1.666
+          expect(left).to.be.closeTo(-1.666, 0.1);
+          // Height matches testHeight, so centered at testTop
+          expect(top).to.equal(0);
         });
       });
     });
